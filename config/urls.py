5102270e00423b70ea816e648bf5ba68a2db557b
Omar Ibañez <OmarIbannez@gmail.com>
2016-09-21 19:53:17 -0700

    Change in admin url

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

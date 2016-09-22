d730c7462b1139e9d46ce46adfb373184e70e465
Omar Ibañez <OmarIbannez@gmail.com>
2016-09-21 19:50:28 -0700

    Extra fields

--- members/admin.py
+++ members/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from members.models import Member
 from members.models import Attend
+from members.models import Division
+from members.models import Moi
+from members.models import Zone
 from django.http import HttpResponse, HttpResponseForbidden
 from members.actions import export_as_csv_action
 
@@ -10,13 +13,34 @@ class MemberAdmin(admin.ModelAdmin):
         'first_name',
         'middle_name',
         'last_name',
+        'division',
+        'moi',
+        'zone',
     )
 
+class DivisionAdmin(admin.ModelAdmin):
+    fields = (
+        'name',
+    )
+
+class MoiAdmin(admin.ModelAdmin):
+    fields = (
+        'name',
+    )
+
+class ZoneAdmin(admin.ModelAdmin):
+    fields = (
+        'name',
+    )
 
 class AttendAdmin(admin.ModelAdmin):
-	list_display = ['attendee', 'date_attend']
-	actions = [export_as_csv_action("CSV Export", fields=['attendee', 'date_attend'])]
+    list_display = ('attendee', 'date_attend', 'division', 'moi', 'zone')
+    list_filter = ('date_attend',)
+    actions = [export_as_csv_action("CSV Export", fields=['attendee', 'date_attend', 'division', 'moi', 'zone'])]
 
 
 admin.site.register(Attend, AttendAdmin)
 admin.site.register(Member, MemberAdmin)
+admin.site.register(Division, DivisionAdmin)
+admin.site.register(Moi, MoiAdmin)
+admin.site.register(Zone, ZoneAdmin)
--- members/models.py
+++ members/models.py
@@ -3,10 +3,40 @@ from django.db import models
 from core.models import BaseModel
 
 
+class Division(BaseModel):
+    name = models.CharField(max_length=255, blank=True, null=True)
+
+    def __unicode__(self):
+        return u'{0}'.format(self.name)
+
+    def __str__(self):
+        return self.__unicode__()
+
+class Moi(BaseModel):
+    name = models.CharField(max_length=255, blank=True, null=True)
+
+    def __unicode__(self):
+        return u'{0}'.format(self.name)
+
+    def __str__(self):
+        return self.__unicode__()
+
+class Zone(BaseModel):
+    name = models.CharField(max_length=255, blank=True, null=True)
+
+    def __unicode__(self):
+        return u'{0}'.format(self.name)
+
+    def __str__(self):
+        return self.__unicode__()
+
 class Member(BaseModel):
     first_name = models.CharField(max_length=255, blank=True, null=True)
     middle_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
+    division = models.ForeignKey(Division, null=True)
+    moi = models.ForeignKey(Moi, null=True)
+    zone = models.ForeignKey(Zone, null=True)
 
     def __unicode__(self):
         return u'{0} {1} {2}'.format(self.first_name,  self.middle_name, self.last_name)
@@ -18,6 +48,16 @@ class Member(BaseModel):
 class Attend(models.Model):
     date_attend = models.DateTimeField(auto_now_add=True)
     attendee = models.ForeignKey(Member)
+
+    def division(self):
+        return self.attendee.division
+
+    def moi(self):
+        return self.attendee.moi
+
+    def zone(self):
+        return self.attendee.zone
+
 
     def __unicode__(self):
         return u'{0} {1} {2} - {3}'.format(self.attendee.first_name, self.attendee.middle_name, self.attendee.last_name, self.date_attend.strftime('%d, %b %Y'))
